70c076fd686825ac6b8c7ae094bb3b1268e471d1
vm947 <vm947@nyu.edu>
2020-05-10 00:56:48 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import Navbar from "./components/layouts/Navbar";
-import UserItem from './components/users/UserItem';
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    
+  };
+  
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
